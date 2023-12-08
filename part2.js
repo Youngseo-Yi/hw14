63f3d1c3397bd5dee290cd0528a029a37a5b92a6
Youngseo-Yi <119606719+Youngseo-Yi@users.noreply.github.com>
2023-12-07 20:46:38 -0500

    Add files via upload

--- part2.js
+++ part2.js
@@ -7,42 +7,56 @@ const MongoUrl = "mongodb+srv://pavomare:PavoMare17@cluster0.5phcbau.mongodb.net
 
 
 
-/*unction find (queryArray) {
+function find (queryArray) {
     MongoClient.connect(url, function(err, db) {
         if(err) { return console.log(err); }
 
         var dbo = db.db("HW14");
-        var collection = dbo.collection('Companies');
-
-        //runQuery(collection, data);
-
-            PSEUDOCODE
-        if queryChoice.value = 'symbol' {
-            db.--.find(symbol = queryChoice.value)    should display all results
+        var coll = dbo.collection('Companies');
+        if (queryArray['queryChoice'] == "coName"){
+            theQuery = {Company:queryArray['textInput']};
+        } else { //queryChoice is symbol
+            theQuery = {Ticker:queryArray['textInput']};
         }
-        if queryChoice.value = 'coName' {
-            db.--.find(company = queryChoice.value)   should display all results
+        coll.find(theQuery).toArray(function(err, items) {
+
+        if (err) {console.log("Error: " + err);}
+        else {
+
+            const options = {
+                sort: { author: 1 },
+                limit: 2,
+                projection: { _id: 0,
+                Company: 1,
+                Ticker: 1,
+                Price: 1},
+                };
+            result = coll.find({},options);
+            if (result.count() === 0) {
+                 console.log("Nothing found!"); 
+            }
         }
+        db.close();
+    
+        });
         
     });
 
-}*/
+}
 
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     urlObj = url.parse(req.url,true)
     path = urlObj.pathname;
-    if (path == "/")
-    {
+    if (path == "/") {
       file="form.html";
       fs.readFile(file, function(err, home) {
       res.write(home);
       res.end();
       })
     }
-    else if (path == "/process")
-    {
+    else if (path == "/process") {
       var body = '';
       req.on('data', chunk => { body += chunk.toString();  });
       req.on('end', () => 
@@ -50,18 +64,17 @@ http.createServer(function (req, res) {
           res.write ("Raw data string: " + body +"<br/>");
           var queryArray= qs.parse(body);
           console.log(queryArray['queryChoice']);
-          if (queryArray['queryChoice'] == 'symbol') {
-            console.log("symbol!!");
+          /*if (queryArray['queryChoice'] == 'symbol') {
+            find()
           } else if (queryArray['queryChoice'] == 'coName') {
             console.log("coName!!");
           } else {
             console.log("unidentified queryChoice");
-          }
+          }*/
           //var radioChoice = body.split('=');
-          var id = queryArray['textInput']
-          console.log(queryArray);
+          //var id = queryArray['textInput'];
 
-          //find(queryArray);
+          find(queryArray);
           });
     }
 }).listen(8080);
