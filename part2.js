736acdb4fcf6996c397178b526a57b1078f63c92
Youngseo-Yi <119606719+Youngseo-Yi@users.noreply.github.com>
2023-12-07 23:37:56 -0500

    Add files via upload

--- part2.js
+++ part2.js
@@ -4,52 +4,13 @@ var url = require('url');
 var http = require('http');
 const MongoClient = require('mongodb').MongoClient;
 const MongoUrl = "mongodb+srv://pavomare:PavoMare17@cluster0.5phcbau.mongodb.net/?retryWrites=true&w=majority";
-
-
-
-function findResults (queryArray) {
-
-    client = new MongoClient(url);
-    console.log('get to here');
-
-    async function doit() {
-    try {
-        await client.connect();
-        console.log('get to here');
-
-        var dbo = client.db("HW14");
-        var coll = dbo.collection('Companies');
-        const options = {
-        projection: { _id: 0, Company: 1, Ticker: 1, Price:1},
-        };
-
-        /*if (queryArray['queryChoice'] =='coName') {
-            myQuery = {Company: queryArray['textInput']};
-        } else {
-            myQuery = {Ticker: queryArray['textInput']};
-        }
-        const curs = coll.find(myQuery,options);
-        // print a message if no documents were found
-        if ((await curs.count()) === 0) {
-        console.log("No documents found!");
-        }
-        console.log(curs);*/
-
-    } 
-    catch(err) {
-        console.log("Database error: " + err);
-    }
-    finally {
-        client.close();
-    }
-    }  //end doit
-    doit();//.catch(console.dir);      
-
-}
-
+var port =process.env.PORT || 3000
+//var port=8080; uncomment to run local
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<link rel="stylsheet" type="text/css" href="formStylesheet.css">');
+
     urlObj = url.parse(req.url,true)
     path = urlObj.pathname;
     if (path == "/") {
@@ -57,17 +18,59 @@ http.createServer(function (req, res) {
       fs.readFile(file, function(err, home) {
       res.write(home);
       res.end();
+
       })
     }
-    else if (path == "/process") {
-      var body = '';
-      req.on('data', chunk => { body += chunk.toString();  });
-      req.on('end', () => { 
-        res.write ("Raw data string: " + body +"<br/>");
-        var queryArray= qs.parse(body);
-        console.log(queryArray['queryChoice']);
-        console.log(queryArray['textInput']);
-        findResults(queryArray);
+    else if (path == "/process") { //check if form has been submitted
+        var body = '';
+        req.on('data', chunk => { body += chunk.toString();  });
+        req.on('end', () => { 
+            var queryArray= qs.parse(body); //get the form data
+
+            //Connect to MongoDB and find the query
+            client = new MongoClient(MongoUrl);
+
+            async function doit() {
+            try {
+                await client.connect();
+
+                var dbo = client.db("HW14");
+                var coll = dbo.collection('Companies');
+                const options = {
+                    projection: { _id: 0, Company: 1, Ticker: 1, Price:1},
+                };
+
+                //check if they are searching company name or ticker
+                if (queryArray['queryChoice'] =='coName') {
+                    myQuery = { Company: { $regex: queryArray['textInput']} }
+                } else {
+                    myQuery = { Ticker: { $regex: queryArray['textInput']} }
+                }
+                const curs = coll.find(myQuery,options);
+                // print a message if no documents were found
+                if ((await curs.count()) === 0) {
+                    console.log("No documents found!");
+                }
+                //write in page for the results
+                await curs.forEach(function(item){
+                    res.write("<div class='myCo'>");
+                    res.write("<div class='Company'>"+ item.Company+"</div>");
+                    res.write("<div class = 'Ticker'>"+item.Ticker+"</div>");
+                    res.write("<div class='Price'>" +item.Price + "</div></div>");
+                });
+                res.end();
+
+            } 
+            catch(err) {
+                console.log("Database error: " + err);
+            }
+            finally {
+                client.close();
+            }
+            }  //end doit
+            doit();
+            //.catch(console.dir);   
         });
     }
-}).listen(8080);
+
+}).listen(port);
